ae4c590fbbcec2c282e7802903b0f17e86cbc546
yangyiliang <yangyiliang@didichuxing.com>
2018-08-05 23:26:08 +0800

    tansition

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h1>transition测试</h1>
+    <p>enter和leave决定动画的开始，leave-to和enter-to决定动画的终点最后，一般是enter和leave-to设置成一样的，然后在active中设置过渡效果</p>
+    <div id="example-1">
+      <button @click="show = !show">
+        Toggle render
+      </button>
+      <transition name="slide-fade">
+        <p v-if="show">hello</p>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      show: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+/* 进入和离开动画可以分别 */
+/* 设置不同的持续时间(duration)和动画函数(timing function) */
+.slide-fade-enter-active {
+  transition: all 3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active 在低于 2.1.8 版本中 */ {
+  transform: translateX(100px);
+  opacity: 0;
+}
+.slide-fade-enter-to
+/* .slide-fade-leave-active 在低于 2.1.8 版本中 */ {
+  transform: translateX(200px);
+  opacity: 1;
+}
+</style>
